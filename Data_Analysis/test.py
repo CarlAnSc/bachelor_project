fd8761bcfd925aa0225bdd89155a2d249d83bc5a
Julius Radzikowski <s204162@dtu.dk>
2023-02-28 14:24:20 +0100

    Begin testing with evaluating on IMageNetX

---
+++ Data_Analysis/test.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torchvision.datasets.imagenet import parse_devkit_archive
+from torchvision import datasets
+import numpy as np
+
+
+np.bool_(1)
+
+datasets.imagenet.parse_devkit_archive('../data/ImageNetVal')
+
